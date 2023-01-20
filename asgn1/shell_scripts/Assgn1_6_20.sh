5189bbaad5d62f0a9805737a4ea20d391c7eb03c
sneaky-potato <ashwanikamal.im421@gmail.com>
2023-01-20 15:30:16 +0530

    Update q6 to include prime factors

--- asgn1/shell_scripts/Assgn1_6_20.sh
+++ asgn1/shell_scripts/Assgn1_6_20.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
-declare -i limit=1000000
+declare -i limit=50
 
 numbers=($(seq 2 $limit))
+spf=($(seq 2 $limit))
 
-for ((i=4; i<=$limit; i+=2))
+for ((i=2; i<=$limit; i+=1))
 do
-    numbers[i-2]=0
+    spf[i-2]=0
 done
 
-for ((i=3; i*i<=$limit; i+=2))
+for ((i=2; i<=$limit; i+=1))
 do
-    if [ ${numbers[i-2]} -ne 0 ]
+    if [ ${spf[i-2]} -eq 0 ]
     then
+        spf[i-2]=$i
         for ((j=i*i; j<=$limit; j+=i))
         do
-            numbers[j-2]=0
+            if [ ${spf[j-2]} -eq 0 ]
+            then
+              spf[j-2]=$i
+            fi
         done
     fi
 done
 
 while read -r line;
 do
-  for val in "${numbers[@]}"
+  while [ $line -ne 1 ]
   do
-    if (( val > 0 && val < line )); then
-      echo -n "${val} " >> "output.txt"
-    fi
+    index=$(($line-2))
+    echo -n "${spf[$index]} " >> "output.txt"
+    line=$(($line/${spf[$index]}))
   done
   echo "" >> "output.txt"
 done < "input.txt"
