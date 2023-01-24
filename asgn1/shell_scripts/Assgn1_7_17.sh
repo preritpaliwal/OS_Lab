b43291684b91f5ed7ac722a09824bf676862c514
sneaky-potato <ashwanikamal.im421@gmail.com>
2023-01-24 18:01:28 +0530

    Fix q7 to output content in original output files

--- asgn1/shell_scripts/Assgn1_7_17.sh
+++ asgn1/shell_scripts/Assgn1_7_17.sh
@@ -1,9 +1,9 @@
-names=$(sort -n ${1}/*)
+names=$(sort ${1}/*)
 mkdir -p $2
 
 touch $2/{a..z}.txt
 
 for n in $names
 do  
-    echo $n >> $2/${n:0:1}.txt
+    echo $n >> $2/$(echo ${n:0:1} | tr '[:upper:]' '[:lower:]').txt
 done
