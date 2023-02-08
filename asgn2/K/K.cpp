455e6478c07ac69227e6ec377b7cf3bb69c7b565
sneaky-potato <ashwanikamal.im421@gmail.com>
2023-02-09 03:16:00 +0530

    Killing entire process group after root

--- asgn2/K/K.cpp
+++ asgn2/K/K.cpp
@@ -6,8 +6,48 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 using namespace std;
+
+int getGroup(int pid)
+{
+    char procname[32];
+    FILE *fp;
+
+    snprintf(procname, sizeof(procname), "/proc/%u/stat", pid);
+    fp = fopen(procname, "r");
+
+    if (fp == NULL)
+    {
+        cerr << "could not open the stat file " << procname << endl;
+        return 0;
+    }
+
+    char c;
+    int group_f = 0;
+    int gpid = 0;
+
+    while ((c = fgetc(fp)) != EOF)
+    {
+        if (c == ' ')
+            group_f++;
+
+        if (group_f == 4)
+        {
+            while ((c = fgetc(fp)) != EOF)
+            {
+                if (c == ' ')
+                    break;
+                gpid = gpid * 10 + (int)(c - '0');
+            }
+            break;
+        }
+    }
+    fclose(fp);
+    return gpid;
+}
 
 int getParent(int pid, char *process)
 {
@@ -62,7 +102,7 @@ int getParent(int pid, char *process)
     return ppid;
 }
 
-int getRoot(int pid)
+pair<int, int> getRoot(int pid)
 {
     char *process = (char *)malloc(32 * sizeof(char));
     char *parentProcess = (char *)malloc(32 * sizeof(char));
@@ -70,13 +110,37 @@ int getRoot(int pid)
     int parent = getParent(pid, process);
     int grandParent = getParent(parent, parentProcess);
 
-    if (parent == 1 || strcmp(process, parentProcess) != 0)
-        return pid;
+    // Base case
+    if (parent <= 1 || strcmp(process, parentProcess) != 0)
+    {
+        try
+        {
+            free(process);
+            free(parentProcess);
 
-    free(process);
-    free(parentProcess);
+            // orphaned child: adopted by init
+            int gpid = getGroup(pid);
+            return {pid, gpid};
+        }
+        catch (...)
+        {
+            cerr << "error killing process " << pid << endl;
+            return {0, 0};
+        }
+    }
 
-    return getRoot(parent);
+    try
+    {
+        free(process);
+        free(parentProcess);
+        // kill(pid, SIGSEGV);
+        return getRoot(parent);
+    }
+    catch (...)
+    {
+        cerr << "error killing process " << pid << endl;
+        return {0, 0};
+    }
 }
 
 int main(int argc, char *argv[])
@@ -96,16 +160,46 @@ int main(int argc, char *argv[])
         cerr << "invalid pid: " << argv[1] << endl;
         exit(EXIT_FAILURE);
     }
-    char *process = (char *)malloc(32 * sizeof(char));
-    int parent = getParent(pid, process);
-    cout << "Parent of " << pid << " (" << process << ") is " << parent << endl;
 
-    int root = getRoot(pid);
-    cout << "Root of " << pid << " is " << root << endl;
+    // create pipe for communication in parent-child
+    int fd[2];
+    pipe(fd);
 
-    // TOOD: read the parent pid of the process with pid from /proc
-    // Write algorithm to find root ppi
+    // fork child
+    pid_t p = fork();
 
-    // print the pid of first parent of the process
+    if (p > 0)
+    {
+        // parent process
+        wait(NULL);
+        close(fd[1]);
+        dup(fd[0]);
+
+        // read recieved root
+        int root;
+        read(fd[0], &root, sizeof(root));
+
+        cout << "Root of " << pid << " is " << root << endl;
+    }
+    else if (p == 0)
+    {
+        close(fd[0]);
+        dup(fd[1]);
+
+        char *process = (char *)malloc(32 * sizeof(char));
+        // get root of bug
+        pair<int, int> p;
+        p = getRoot(pid);
+
+        cout << "killing -" << p.second << endl;
+        kill(-1 * p.second, SIGSEGV);
+        // send back to parent process
+        write(fd[1], &p.second, sizeof(p.first));
+    }
+    else
+    {
+        perror("fork error");
+    }
+
     return 0;
 }
--- asgn2/K/trojan.c
+++ asgn2/K/trojan.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
-#define MAX_PAR_CHILD 5
-#define MAX_CHILD_CHILD 10
-#define SLEEP_TIME 120
+#define MAX_PAR_CHILD 2
+#define MAX_CHILD_CHILD 2
+#define SLEEP_TIME 5
 
 void attack()
 {
@@ -15,27 +15,25 @@ void attack()
 int main()
 {
     printf("root pid: %d", getpid());
-    while (1)
+    sleep(SLEEP_TIME);
+    for (int i = 0; i < MAX_PAR_CHILD; i++)
     {
-        sleep(SLEEP_TIME);
-        for (int i = 0; i < MAX_PAR_CHILD; i++)
+        pid_t p = fork();
+        if (p == 0)
         {
-            pid_t p = fork();
-            if (p == 0)
+            printf("trojan child: %d", getpid());
+            for (int i = 0; i < MAX_CHILD_CHILD; i++)
             {
-                printf("trojan child: %d", getpid());
-                for (int i = 0; i < MAX_CHILD_CHILD; i++)
+                pid_t pid = fork();
+                if (pid == 0)
                 {
-                    pid_t pid = fork();
-                    if (pid == 0)
-                    {
-                        printf("trojan grandchild: %d", getpid());
-                        attack();
-                    }
+                    printf("trojan grandchild: %d", getpid());
+                    attack();
                 }
-                attack();
             }
+            attack();
         }
     }
+    sleep(SLEEP_TIME);
     return 0;
 }
