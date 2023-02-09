e92d19a97e25aa86f3b32de3ce1b3ce88775e85c
sneaky-potato <ashwanikamal.im421@gmail.com>
2023-02-09 14:31:26 +0530

    Kill locking processes

--- asgn2/I/I.cpp
+++ asgn2/I/I.cpp
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 using namespace std;
 
@@ -20,6 +21,10 @@ int main(int argc, char *argv[])
     }
     int fd[2];
     pipe(fd);
+
+    if (fcntl(fd[0], F_SETFL, O_NONBLOCK) < 0)
+        exit(2);
+
     pid_t p = fork();
 
     if (p > 0)
@@ -29,13 +34,26 @@ int main(int argc, char *argv[])
         close(fd[1]);
         dup(fd[0]);
 
-        // read recieved root
+        // read recieved pids
         int *pids = (int *)malloc(100 * sizeof(int));
         for (int i = 0; i < 100; i++)
             pids[i] = -1;
 
-        read(fd[0], pids, sizeof(pids));
+        char buffer[1024];
 
+        while (read(fd[0], buffer, sizeof(buffer)) != 0)
+        {
+            char *token = strtok(buffer, " ");
+            int i = 0;
+            while (token != NULL)
+            {
+                pids[i] = atoi(token);
+                token = strtok(NULL, " ");
+                i++;
+            }
+        }
+
+        // print pids
         cout << "processes locking file:"
              << "\n";
         for (int i = 0; pids[i] > 0; i++)
@@ -57,12 +75,16 @@ int main(int argc, char *argv[])
     }
     else if (p == 0)
     {
+        close(1);
         close(fd[0]);
         dup(fd[1]);
 
         // get pids locking on the file
         int *pids = (int *)malloc(100 * sizeof(int));
-        write(fd[1], pids, sizeof(pids));
+        if (execlp("fuser", "fuser", argv[1], (char *)NULL) < 0)
+        {
+            perror("execlp error");
+        }
     }
     else
     {
